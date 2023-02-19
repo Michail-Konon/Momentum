b6a9d32065f970ac6bc24c55337f52ed9c8622c2
michail-konon <greateyuri@gamil.com>
2023-02-19 23:05:33 +0300

    feat: upload audio player prototype

---
+++ playList.js
@@ -0,0 +1,38 @@
+const playList = [
+    {      
+      title: 'Aqua Caelestis',
+      src: './assets/sounds/Aqua Caelestis.mp3',
+      duration: '00:58'
+    },  
+    {      
+      title: 'River Flows In You',
+      src: './assets/sounds/River Flows In You.mp3',
+      duration: '03:50'
+    }
+    ,  
+    {      
+      title: 'The Pit',
+      src: './assets/sounds/The Pit.mp3',
+      duration: '03:00'
+    }
+    ,  
+    {      
+      title: 'Catacombs',
+      src: './assets/sounds/Catacombs.mp3',
+      duration: '02:44'
+    }
+    ,  
+    {      
+      title: 'Serpent Yards',
+      src: './assets/sounds/Serpent Yards.mp3',
+      duration: '03:05'
+    }
+    ,  
+    {      
+      title: 'Sierra De Chiapas (Palenque)',
+      src: './assets/sounds/Sierra De Chiapas (Palenque).mp3',
+      duration: '02:35'
+    }
+  ]
+  export default playList;
+  
--- sctipt.js
+++ sctipt.js
@@ -1,7 +1,10 @@
-// Weather API Key 98e20b6373849b7ff4b0fd01a78b1a87
-//https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=ru&appid=98e20b6373849b7ff4b0fd01a78b1a87&units=metric
+import playList from './playList.js';
+console.log(playList);
+let arr;
+
 const time = document.querySelector('.time');        // time
 const actualDate = document.querySelector('.date'); // date 
+const API_KEY = '98e20b6373849b7ff4b0fd01a78b1a87';
 
 const greeting = document.querySelector('.greeting');    // Greeting
 const userName = document.querySelector('.name');       // local storage
@@ -20,12 +23,162 @@ const errorMsg = document.querySelector('.weather-error')                 // wea
 const wind = document.querySelector('.wind')                             // weather
 const humidity = document.querySelector('.humidity')                    // weather
 
-const changeQuoteBtn = document.querySelector('.change-quote') // quotes
-const quoteContent = document.querySelector('.quote')         // quotes
-const autorContent = document.querySelector('.author')       // quotes
+const changeQuoteBtn = document.querySelector('.change-quote')  // quotes
+const quoteContent = document.querySelector('.quote')          // quotes
+const autorContent = document.querySelector('.author')        // quotes
+
+const controlStop = document.querySelector('.stop')          // audio player
+const controlPlay = document.querySelector('.play')         // audio player
+const controlPrev = document.querySelector('.play-prev')   // audio player
+const controlNext = document.querySelector('.play-next')  // audio player
+const playlistContainer = document.querySelector('.play-list')    // audio player
+
 
 let randNum = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
 let language = 'ru'
+let isPlayed = false;
+let playNum = 0;
+
+/*Audio Player START*/    
+
+const audio = new Audio();
+audio.src = playList[playNum].src;
+
+audio.onended = () => {
+    if(playNum == (playList.length - 1)) {
+        arr[playNum].classList.remove('play-item');
+        playNum = 0
+        arr[playNum].classList.add('play-item');
+        audio.src = playList[playNum].src;
+        audio.play();
+        console.log('end and next should be first');
+    } else {
+        arr[playNum].classList.remove('play-item');
+        playNum += 1;
+        arr[playNum].classList.add('play-item');
+        audio.src = playList[playNum].src;
+        audio.play();
+        console.log(`next is ${playNum}`);
+    } 
+
+};
+
+// 1) сделать перелистывание << >> DONE
+// 2) формирование плейлиста через JS  
+// 3) добавление новых треков
+// 4) cделать шкалу прогресса через ползунок, с ежесекундным обновлением
+// 5) На него ухо и функцию, которая переводит длительность в проценты или как то так
+// 6) сделать бегущую строку как в AIMP
+controlPrev.addEventListener('click', () => {
+    if(isPlayed == true) {
+        isPlayed = false;
+        controlPlay.classList.toggle('pause');
+        nextTrack('-1');
+    } else {
+        nextTrack('-1');
+    }
+    console.log(playNum);
+})
+controlNext.addEventListener('click', () => {
+    if(isPlayed == true) {
+        isPlayed = false;
+        controlPlay.classList.toggle('pause');
+        nextTrack('1');
+    } else {
+        nextTrack('1');
+    }
+    console.log(playNum);
+})
+
+function nextTrack(direct) {
+    if(playNum == (playList.length - 1) && direct == '1') {  
+        arr[playNum].classList.remove('play-item');
+        playNum = 0;
+        audio.src = playList[playNum].src;
+    } else if(playNum == 0 && direct == '-1') {
+        arr[playNum].classList.remove('play-item');
+        playNum = (playList.length - 1);
+        audio.src = playList[playNum].src;
+    } else {
+        arr[playNum].classList.remove('play-item');
+        playNum += Number(direct);
+        audio.src = playList[playNum].src;
+    } 
+}
+
+controlPlay.addEventListener('click', () => {
+    audioPlayer();
+    controlPlay.classList.toggle('pause');
+})
+
+controlStop.addEventListener('click', () => {
+    if(isPlayed == true) {
+        audio.currentTime = 0;
+        audio.pause();
+        isPlayed = false;
+        controlPlay.classList.toggle('pause');
+        arr.forEach((el) => {
+            el.classList.remove('play-item');
+        });
+    } else {
+        audio.currentTime = 0;
+    }
+})
+
+
+
+function createPlayList() {
+    let liArr = [];
+    playList.forEach((el, i) => {
+        const li = document.createElement('li');
+        li.textContent = `${i + 1}) ${playList[i].title}`;
+        li.classList.add('player-item');
+        playlistContainer.appendChild(li);
+
+        li.addEventListener('click', ()=> {
+             if(li.classList.contains('play-item')) { 
+                audio.src = playList[i].src;
+                li.classList.toggle('play-item');
+                isPlayed = false;
+                controlPlay.classList.toggle('pause');
+                console.log(`same , index = ${playList[playNum].title} and current = ${playList[i].title} ${isPlayed}`);
+            } else if(isPlayed == true && playList[playNum].title != playList[i].title) {
+                liArr.forEach((el) => {
+                    el.classList.remove('play-item');
+                });
+                li.classList.toggle('play-item');
+                audio.src = playList[i].src;
+                audio.play();
+                playNum = i;
+                console.log(`different, index = ${playList[playNum].title} and current = ${playList[i].title} ${isPlayed}`);
+            } else {
+                isPlayed = true;
+                audio.src = playList[i].src;
+                controlPlay.classList.toggle('pause');
+                audio.play();
+                li.classList.toggle('play-item');
+                playNum = i;
+                console.log(`false, index = ${playList[playNum].title} and current = ${playList[i].title} ${isPlayed}`);
+            }
+        });
+        liArr.push(li);
+    });
+    return arr = liArr
+}
+window.addEventListener('load', createPlayList)
+
+function audioPlayer() {
+    if(isPlayed == false) {
+        audio.play();
+        isPlayed = true
+        arr[playNum].classList.toggle('play-item');
+    } else {
+        audio.pause()
+        isPlayed = false
+        arr[playNum].classList.toggle('play-item');
+    }
+}
+/*Audio Player END*/
 
 /*Quotes START*/
 
@@ -55,7 +208,7 @@ let weatherArr = [temperature, weatherDescription, humidity, wind]
 
 async function getWeather() {
     let city = localStorage.getItem('city');
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${language}&appid=98e20b6373849b7ff4b0fd01a78b1a87&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${language}&appid=${API_KEY}&units=metric`
     const res = await fetch(url);
     const data = await res.json(); 
     if( data.cod == "404") {
